c9a20aebed6b985511e60acbb35cd817c4ae8743
wdeucher <william.deucher@tufts.edu>
2020-07-09 11:13:06 -0400

    first draft of landing page. Still has problems resizing for mobile and needs updated logo & text.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,32 @@
+import styles from '../components/landing_page.module.css'
+
 function HomePage() {
-  return <div>Hello World!</div>;
+  return (
+  	<div className={styles.body}>
+  		<div className={styles.row}>
+	  		<div className={styles.column}>
+		  		<div className={styles.TCFG}>Tufts Code for Good</div>
+		  		<p className={styles.text}> 
+		  			Insert description of Tufts Code For Good. "Lorem ipsum 
+		  			dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+		  			tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+		  			ad minim veniam, quis nostrud exercitation ullamco laboris 
+		  			nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+		  			dolor in reprehenderit in voluptate velit esse cillum 
+		  			dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+		  			cupidatat non proident, sunt in culpa qui officia deserunt 
+		  			mollit anim id est laborum."
+  				</p>
+		  		<button type="button" className={styles.Rectangle}> 
+		  			<div className={styles.LearnMore}> Learn More </div> 
+		  		</button>
+	  		</div>
+	  		<div className={styles.column}>
+	  			<img src="/images/TempLogo.jpg" alt="Tufts Code For Good Logo" className={styles.img} />
+	  		</div>
+  		</div>
+	</div>
+  );
 }
 
 export default HomePage;
