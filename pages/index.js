cc7825fac850e6cda2fd2776ee2f221c2276d33d
wdeucher <william.deucher@tufts.edu>
2020-07-13 18:09:11 -0400

    resizing for mobile

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function HomePage() {
   	<div className={styles.body}>
   		<div className={styles.row}>
 	  		<div className={styles.column}>
-		  		<div className={styles.TCFG}>Tufts Code for Good</div>
+		  		<h1 className={styles.TCFG}>Tufts Code <br/> for Good</h1>
 		  		<p className={styles.text}> 
 		  			Insert description of Tufts Code For Good. "Lorem ipsum 
 		  			dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
