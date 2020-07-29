57c717865d779617b0fef3e1875ac6732b83dead
wdeucher <william.deucher@tufts.edu>
2020-07-29 11:39:24 -0400

    move page to projects.js file

---
+++ pages/projects.js
@@ -0,0 +1,58 @@
+import s from '../components/project_list.module.css'
+
+function Projects() {
+  return (
+  	<div className={s.body} id={s.bg}>
+  		<div id={s.text_container}>
+	  		<h1 className={s.txt} id={s.title}>PROJECTS</h1>
+	  		<h3 className={s.txt} id={s.sub}>TUFTS CODE FOR GOOD</h3>
+  		</div>
+  		<div className={s.column}>
+  			<div className={s.item}>
+  				<img src="/images/projects_logos/example1.jpg" alt="1" className={s.logo} />
+			</div>
+			<div className={s.item}>
+  				<img src="/images/projects_logos/example2.jpg" alt="2" className={s.logo} />
+  			</div>
+  			<div className={s.item}>
+  				<img src="/images/projects_logos/example3.jpg" alt="3" className={s.logo} />
+  			</div>
+  		</div>
+   		<div className={s.column}>
+  			<div className={s.item}>
+  				<img src="/images/projects_logos/example3.jpg" alt="3" className={s.logo} />
+  			</div>
+  			<div className={s.item}>
+  				<img src="/images/projects_logos/example2.jpg" alt="2" className={s.logo} />
+  			</div>
+  			<div className={s.item}>
+  				<img src="/images/projects_logos/example1.jpg" alt="1" className={s.logo} />
+  			</div>
+  		</div>
+    	<div className={s.column}>
+  			<div className={s.item}>
+  				<img src="/images/projects_logos/example2.jpg" alt="2" className={s.logo} />
+  			</div>
+  			<div className={s.item}>
+  				<img src="/images/projects_logos/example1.jpg" alt="1" className={s.logo} />
+  			</div>
+  			<div className={s.item}>
+  				<img src="/images/projects_logos/example3.jpg" alt="3" className={s.logo} />
+  			</div>
+		</div>
+    	<div className={s.column}>
+  			<div className={s.item}>
+  				<img src="/images/projects_logos/example2.jpg" alt="2" className={s.logo} />
+  			</div>
+  			<div className={s.item}>
+  				<img src="/images/projects_logos/example1.jpg" alt="1" className={s.logo} />
+  			</div>
+  			<div className={s.item}>
+  				<img src="/images/projects_logos/example3.jpg" alt="3" className={s.logo} />
+  			</div>
+		</div>
+	</div>
+  );
+}
+
+export default Projects;
