a8052cb7f2b048be33cc9a1d5191ebf7a969fc21
wdeucher <william.deucher@tufts.edu>
2020-07-29 12:09:43 -0400

    first draft. integrate projects list page with nav bar

--- pages/projects.js
+++ pages/projects.js
@@ -1,34 +1,74 @@
 import styles from '../components/landing_page.module.css'
+import s from '../components/project_list.module.css'
 
 function Projects() {
   return (
-    <div className={styles.body}>
-      <div className={styles.row}>
-        <div className={styles.column}>
+    <div className={s.body} id={s.bg}>
+      <div className={styles.body}>
+        <div className={styles.row}>
+          <div className={styles.column}>
 
-         <div className={styles.nav_bar}>
-            <a href ="/"><img src="/images/TempLogo.jpg" alt="Tufts Code For
-            Good Logo" width= "181px" height="61px" border="solid 1px #979797"/>
-            </a>
-            <a href="/about" className={styles.nav_bar}>ABOUT</a>
-            <a href="/projects" className={styles.nav_bar}>PROJECTS</a>
-            <a href="/funding" className={styles.nav_bar}>FUNDING</a>
+           <div className={styles.nav_bar}>
+              <a href ="/"><img src="/images/TempLogo.jpg" alt="Tufts Code For
+              Good Logo" width= "181px" height="61px" border="solid 1px #979797"/>
+              </a>
+              <a href="/about" className={styles.nav_bar}>ABOUT</a>
+              <a href="/projects" className={styles.nav_bar}>PROJECTS</a>
+              <a href="/funding" className={styles.nav_bar}>FUNDING</a>
+            </div>
           </div>
-
-          <h1 className={styles.TCFG}>Projects</h1>
-          <p className={styles.text}>
-            "Lorem ipsum  dolor sit amet, consectetur adipiscing elit, sed do
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum
-            dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum."
-          </p>
-
         </div>
       </div>
+      
+        <div id={s.text_container}>
+          <h1 className={s.txt} id={s.title}>PROJECTS</h1>
+          <h3 className={s.txt} id={s.sub}>TUFTS CODE FOR GOOD</h3>
+        </div>
+        <div className={s.column}>
+          <div className={s.item}>
+            <img src="/images/projects_logos/example1.jpg" alt="1" className={s.logo} />
+          </div>
+          <div className={s.item}>
+            <img src="/images/projects_logos/example2.jpg" alt="2" className={s.logo} />
+          </div>
+          <div className={s.item}>
+            <img src="/images/projects_logos/example3.jpg" alt="3" className={s.logo} />
+          </div>
+        </div>
+        <div className={s.column}>
+          <div className={s.item}>
+            <img src="/images/projects_logos/example3.jpg" alt="3" className={s.logo} />
+          </div>
+          <div className={s.item}>
+            <img src="/images/projects_logos/example2.jpg" alt="2" className={s.logo} />
+          </div>
+          <div className={s.item}>
+            <img src="/images/projects_logos/example1.jpg" alt="1" className={s.logo} />
+          </div>
+        </div>
+        <div className={s.column}>
+          <div className={s.item}>
+            <img src="/images/projects_logos/example2.jpg" alt="2" className={s.logo} />
+          </div>
+          <div className={s.item}>
+            <img src="/images/projects_logos/example1.jpg" alt="1" className={s.logo} />
+          </div>
+          <div className={s.item}>
+            <img src="/images/projects_logos/example3.jpg" alt="3" className={s.logo} />
+          </div>
+        </div>
+        <div className={s.column}>
+          <div className={s.item}>
+            <img src="/images/projects_logos/example2.jpg" alt="2" className={s.logo} />
+          </div>
+          <div className={s.item}>
+            <img src="/images/projects_logos/example1.jpg" alt="1" className={s.logo} />
+          </div>
+          <div className={s.item}>
+            <img src="/images/projects_logos/example3.jpg" alt="3" className={s.logo} />
+          </div>
+        </div>
+
     </div>
   );
 }
