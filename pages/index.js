b0eb5d218dcc0e84df87ce7562546b8093eac450
wdeucher <william.deucher@tufts.edu>
2020-08-03 16:12:40 -0400

    update Learn More button

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 import styles from '../components/landing_page.module.css'
+import Link from 'next/link';
 
 function HomePage() {
   return (
   	<div className={styles.body}>
   		<div className={styles.row}>
 	  		<div className={styles.column}>
-		  		<h1 id={styles.TCFG}>Tufts Code <br/> for Good</h1>
-		  		<button type="button" className={styles.Rectangle}>
-		  			<div id={styles.LearnMore}> Learn More </div>
-		  		</button>
+		  		<h1 id={styles.TCFG}>TUFTS CODE <br/> FOR GOOD</h1>
+		  		<Link href="/about">
+	  				<div id={styles.learnContainer}>
+	  					<span id={styles.learn}> Learn more </span>
+  						<div id={styles.arrowContainer}>
+  							<div className={styles.arrow} />
+						</div>
+					</div>
+		  		</Link>
 	  		</div>
 	  		<div className={styles.column}>
 	  			<img src="/images/Logo.jpg" alt="Tufts Code For Good Logo" className={styles.img} />
